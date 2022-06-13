7f4ae254b8862308b1408fbed396b666a99a17db
Hilal-Almoqbali <hilalalmoqbali2060@gmail.com>
2022-06-13 10:59:37 +0400

    changes

--- src.glad/shaders.cc
+++ src.glad/shaders.cc
@@ -197,37 +197,32 @@ static unsigned int LinkShader(unsigned int vertexShader,unsigned int fragmentSh
     return shaderProgram;
 }
 
-static ShadderSource ParseShader(const char* filePath)
+static struct ShadderSource ParseShader(const std::string& filepath)
 {
-    std::ifstream stream(filePath);
-
     enum class ShaderType
     {
-        NONE =-1,VERTEX = 0,FRAGMENT = 1
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
     };
 
+    std::ifstream stream(filepath);
     std::string line;
     std::stringstream ss[2];
     ShaderType type = ShaderType::NONE;
-    while(getline(stream,line))
+
+    while (getline(stream, line))
     {
-        if(line.find("#shader") != std::string::npos)
+        if (line.find("#shader") != std::string::npos)
         {
-            if(line.find("vertex") != std::string::npos)
-            {
-                //set mode to vertex
+            if (line.find("vertex") != std::string::npos)
                 type = ShaderType::VERTEX;
-            }
-            if(line.find("fragment") != std::string::npos)
-            {
-                //set mode to fragment
+            else if (line.find("fragment") != std::string::npos)
                 type = ShaderType::FRAGMENT;
-            }
-            else
-            {
-                ss[(int)type] << line <<"\n";
-            }
+        }
+        else
+        {
+            ss[(int)type] << line << '\n';
         }
     }
-    return {ss[0].str(),ss[1].str()};
+
+    return { ss[0].str(), ss[1].str() };
 }
