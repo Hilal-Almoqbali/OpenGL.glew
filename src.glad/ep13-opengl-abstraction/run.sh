0edd1979aaf24e0f5df45ec92cab8108bd5558e4
Hilal-Almoqbali <hilalalmoqbali2060@gmail.com>
2022-06-13 10:55:12 +0400

    changes

---
+++ src.glad/ep13-opengl-abstraction/run.sh
@@ -0,0 +1,2 @@
+g++ -o ./bin/main.bin ./Application.cpp ./IndexBuffer.cpp ./Renderer.cpp VertexBuffer.cpp -I ./ -lglfw -lGLEW -lGL -lm -lXrandr -lXi -lX11 -lXxf86vm -lpthread -ldl -lXinerama -lXcursor
+./bin/main.bin
