09f931cb8e4e092bfb41c08483d132d87a354051
Hilal-Almoqbali <hilalalmoqbali2060@gmail.com>
2022-06-14 19:00:35 +0400

    changes

--- src.glad/run.sh
+++ src.glad/run.sh
@@ -1,2 +1,2 @@
-g++ -o ../bin/main.bin ../compile/glad.c ./indexbuffe.cc -I ../include/ -lglfw -lGLEW -lGL -lm -lXrandr -lXi -lX11 -lXxf86vm -lpthread -ldl -lXinerama -lXcursor
+g++ -o ../bin/main.bin ../compile/glad.c ./shaders.cc -I ../include/ -lglfw -lGLEW -lGL -lm -lXrandr -lXi -lX11 -lXxf86vm -lpthread -ldl -lXinerama -lXcursor
 ../bin/main.bin
